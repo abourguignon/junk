b29eb14440b7e41f8d1b0a45a07f965d227b4e98
Antonin Bourguignon <antonin.bourguignon@gmail.com>
2014-06-19 15:11:29 +0200

    Handle options with optparse

--- count.py
+++ count.py
@@ -1,43 +1,33 @@
 import sys
+from optparse import OptionParser
 
-# Defaults
-result_list_size = 20
-depth = 2
+# Args
+parser = OptionParser(description="Dummy command line stats tool")
+parser.add_option("-f", "--file", type="string", dest="filename", help="the command line history file")
+parser.add_option("-d", "--depth", type="int", dest="depth", default=2, help="depth of commands parsing")
+parser.add_option("-l", "--length", type="int", dest="output_length", default=10, help="output length")
 
-# Handle args
-if len(sys.argv) > 1:
-    path = str(sys.argv[1])
-else:
-    raise Exception('No path given')
-
-if len(sys.argv) > 2:
-    try:
-        result_list_size = int(sys.argv[2])
-    except Exception as e:
-        raise Exception('Second argument (result list size / default %s) must be an int (%s)' % (result_list_size, e))
-
-if len(sys.argv) > 3:
-    try:
-        depth = int(sys.argv[3])
-    except Exception as e:
-        raise Exception('Third argument (depth / default %s) must be an int (%s)' % (depth, e))
+try:
+    options, args = parser.parse_args()
+except:
+    exit(-1)
 
 # Count
-f = open(path, 'r')
+f = open(options.filename, 'r')
 
 count = {}
 max_size = 0
 for e in f.readlines():
     # Only take the `depth` first words into account
-    cmd = ' '.join(e.split(' ')[:depth]).rstrip()
+    cmd = ' '.join(e.split(' ')[:options.depth]).rstrip()
     count[cmd] = count[cmd] + 1 if cmd in count else 1
 
-sorted_truncated_count = sorted(count, key=count.get, reverse=True)[:result_list_size]
+sorted_truncated_count = sorted(count, key=count.get, reverse=True)[:options.output_length]
 
 # Output
 # Determine max size for output padding
 max_cmd_length = len(max(sorted_truncated_count, key=len))
-print "Your %s top used commands (depth %s)\n" % (result_list_size, depth)
+print "Your %s top used commands (depth %s)\n" % (options.output_length, options.depth)
 for cmd in sorted_truncated_count:
     padding = (max_cmd_length - len(cmd)) * ' '
     print '%s %s %s' % (cmd, padding, count[cmd])
