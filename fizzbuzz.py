1981aa61d92c8575fbc59a53d73845729b297124
Antonin Bourguignon <antonin.bourguignon@gmail.com>
2014-07-22 13:07:43 +0200

    Dummy Fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,27 @@
+"""
+For a range of numbers going from 1 to 100: 
+- if the number is a multiple of 3 or contains the number 3, print "Fizz"
+- if the number is a multiple of 5 or contains the number 5, print "Buzz"
+- otherwise print the number
+"""
+
+output = ''
+for i in range(1, 100):
+    print_number = True
+
+    if i % 3 == 0 or '3' in str(i):
+        output += 'Fizz'
+        print_number = False
+    if i % 5 == 0 or '5' in str(i):
+        output += 'Buzz'
+        print_number = False
+
+    if print_number:
+        output += str(i)
+
+    output += '\n'
+
+# Following the preconditions, 3 and 5 shouldn't occur in the final output
+assert('3' not in output and '5' not in output)
+
+print output
